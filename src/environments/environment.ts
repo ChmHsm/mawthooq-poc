9e119ae8ea3670cb72084bf4f3869e2027f1695f
houssamc <houssamc@blackstoneeit.com>
2022-10-11 20:42:51 +0400

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBiqimBTtswxmcN7i559A5zDR9WrBJjynk",
+  authDomain: "mawthooq-poc.firebaseapp.com",
+  projectId: "mawthooq-poc",
+  storageBucket: "mawthooq-poc.appspot.com",
+  messagingSenderId: "735246766224",
+  appId: "1:735246766224:web:bf04e10077bbaf513ec815",
+  measurementId: "G-VGWQDYW9ZR",
+    
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    
+  }
+};
